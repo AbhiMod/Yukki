2717e7a0b07240055722a15853d169a850198eec
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-21 23:06:43 +0530

    Update reload.py

--- AnieMusic/plugins/tools/reload.py
+++ AnieMusic/plugins/tools/reload.py
@@ -94,7 +94,7 @@ async def close_menu(_, query: CallbackQuery):
         umm = await query.message.reply_text(
             f"Cʟᴏsᴇᴅ ʙʏ : {query.from_user.mention}"
         )
-        await asyncio.sleep(7)
+        await asyncio.sleep(2)
         await umm.delete()
     except:
         pass
