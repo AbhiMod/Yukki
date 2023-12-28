11a9cf9a3fec2df815874af8185004d8ca5eda95
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-28 11:01:54 +0530

    Update and rename am.py to am

--- am.py
+++
@@ -1,4 +0,0 @@
-from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf Yukki && git clone https://github.com/AbhiMod/Yukki && cd Yukki && python3 -m AnieMusic")
-# try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
