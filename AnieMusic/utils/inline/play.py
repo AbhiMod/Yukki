9a456cb3fc78d5094e418480896800f4cb9684e3
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-04 23:39:53 +0530

    Update play.py

--- AnieMusic/utils/inline/play.py
+++ AnieMusic/utils/inline/play.py
@@ -54,12 +54,6 @@ def stream_markup_timer(_, chat_id, played, dur):
     else:
         bar = "—————————◉"
     buttons = [
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
          [
             InlineKeyboardButton(
                 text=_["S_B_3"],
